745715856335ee25762cceb7922292fd2a272fab
Mattheus Gomes <macttheusag@yahoo.com.br>
2020-07-14 18:10:20 -0300

    commit Finalização do projeto.

--- app_orcamentoPessoal/app.js
+++ app_orcamentoPessoal/app.js
@@ -56,6 +56,7 @@ class Bd {
                 continue
             } 
 
+            despesa.id = i
             despesas.push(despesa)
         }
         return despesas
@@ -98,6 +99,10 @@ class Bd {
         }
 
         return despesasFiltradas
+    }
+
+    remover(id) {
+        localStorage.removeItem(id)
     }
 }
 
@@ -195,6 +200,22 @@ function carregaListaDespesas(despesas = Array(), filtro = false){
 
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
+
+        //criando botão de exclusão
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            // remover a despesa
+            let id = this.id.replace('id_despesa_', '')
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
+        console.log(d)
 
     })
 
